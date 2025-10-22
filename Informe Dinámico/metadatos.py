feef0463b938ad6f7f8d9dfb8504a338aca13051
Rickfer14 <rickqf14@gmail.com>
2025-10-22 02:22:08 -0400

    Actualizacion

--- Informe Dinámico/metadatos.py
+++ Informe Dinámico/metadatos.py
@@ -6,8 +6,8 @@ with open('informe.ipynb', "r", encoding="utf-8") as f:
 notebook.metadata["title"] = "ANÁLISIS DE LA EVOLUCIÓN Y DESIGUALDAD DEL CONSUMO DE ENERGÍA" \
 " PER CÁPITA EN LOS PAÍSES DE AMÉRICA DEL SUR ENTRE 1980 Y 2013, " \
 "EVALUANDO SU RELACIÓN CON EL PIB PER CÁPITA."
-notebook.metadata["date"] = "Ricardo Quintero, Arturo Borrero"
-notebook.metadata["author"] = "Ricardo Quintero, Arturo Borrero"
+notebook.metadata["date"] = "Arturo Borrero, Ricardo Quintero"
+notebook.metadata["author"] = "Arturo Borrero, Ricardo Quintero"
 
 with open('informe.ipynb', "w", encoding="utf-8") as f:
     nbformat.write(notebook, f)
